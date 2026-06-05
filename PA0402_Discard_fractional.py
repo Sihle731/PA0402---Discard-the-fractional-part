e2c163180ada7b5aecd2624c2673672bcd66e510
Andile Andre3000 <lasmkhwanazi@gmail.com>
2026-06-05 23:31:28 +0100

    Add files via upload

---
+++ PA0402_Discard_fractional.py
@@ -0,0 +1,19 @@
+##Question 1.1
+
+import math
+
+def min_rooms_needed(guests: int) -> int:
+    if guests < 0:
+        raise ValueError("Number of guests cannot be negative")
+    return math.ceil(guests / 3)
+
+# Example
+# print(min_rooms_needed(10))  # output: 4
+
+##Question 1.2
+
+def generate_subnets():
+    return [f"192.168.1.{i*32}/27" for i in range(8)]
+
+for subnet in generate_subnets():
+    print(subnet)
